1a9b778a15568dcaa6a499b09fb4d5a22ba61ce5
yuhyun <dbgus101@gmail.com>
2021-02-14 22:31:12 +0900

    feat go first

---
+++ main.go
@@ -0,0 +1,37 @@
+package main // main 은 컴파일을 위해서 필요하다. 공유 라이브러리를 만들거면 main xx
+// 익스텐션을 설치하면 자동 줄바꿈 자동 타입체크 등등 다해줌 겁나 편한가?
+// 함수 이름의 시작이 소문자면 내부함수 대문자면 import 가능
+// 가끔씩 편집기 창이 겁나 춤춤... 이상함 ㅠ
+import (
+	"fmt"
+	"strings"
+	"time"
+)
+
+func mul(a int, b int) int { // 각각각 타입을 지정해 줘야 함
+	return a * b
+}
+func returnType(name string) (length int, uppercase string) {
+	defer fmt.Print("\n imDone\n") // fnc가 끝나면 동작함
+	length = len(name)
+	uppercase = strings.ToUpper(name)
+	return // 요런 신기한것도 지원함 ㄷㄷ 이걸 naked return 이라고 함
+}
+func main() {
+	const name string = "yh" // 정적 타입 지정 언어
+	myName := "leeyh"        // 축약형을 쓰면 첫번째로 지정한 타입으로 할당됨 지금은 str
+	myName = "cococo"        // 타입을 지정하지 않으면 프프린트도 안댐
+	fmt.Println("hello, world")
+	fmt.Println("\n할로 월드?")
+	fmt.Println(name + "\n\n")
+	fmt.Println(myName)
+	fmt.Println(mul(2, 3))
+	fmt.Println(returnType("sass")) //뭔가 달고 주석추가 안하면 코딩창 지랄남?
+	fmt.Println(time.Now())
+	result := 0
+	for i := 0; i < 1000000; i++ {
+		result += i
+	}
+	fmt.Println(result)
+	fmt.Println(time.Now())
+}
